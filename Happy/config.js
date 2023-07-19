6722989ace69d5aed72b7c26d90d9c69e3dbec65
meohoipeo249 <111524703+meohoipeo249@users.noreply.github.com>
2023-07-20 00:01:34 +0700

    Add files via upload

---
+++ Happy/config.js
@@ -0,0 +1,13 @@
+const nameGirl = '18 Tủi';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday Chị Gái ';
+const titleCard = 'Tặng Chị Gái';
+const contentCard = 'Chúc Bà Chị 18 tủi vẫn mãi xênh bớt giận hờn và yêu thằng em này nhiều hơn nha';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Lonapham.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
